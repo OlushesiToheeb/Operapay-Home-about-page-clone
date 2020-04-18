49ecf0daa44d1e23d027510973aa5e116d243e00
OlushesiToheeb <olaolushesi98@gmail.com>
2020-04-18 01:08:05 +0100

    :sweat: ft - added more responsive breakpoint and mobile menu

--- index.js
+++ index.js
@@ -15,13 +15,35 @@ window.addEventListener("load", () => {
 
 })
 
+
+// toggle the service dropdown to toggle the sub-menu
+
 let serviceDropdown = document.getElementById('service');
 let dropdown = document.getElementById('dropdown');
 let angleUp = document.getElementById('angleUp')
 let angleDown = document.getElementById('angleDown');
 
 serviceDropdown.addEventListener('click', () => {
+    alert('hello');
     dropdown.classList.toggle('d-none');
     angleDown.classList.toggle('d-none')
     angleUp.classList.toggle('d-none')
+})
+
+// nav for mobile responsive 
+
+let mobileMenuToggler = document.getElementById('toggle-button')
+let mobilemenu = document.getElementById('mobile-menu')
+
+mobileMenuToggler.addEventListener('click', () =>{
+    
+    let show = false
+
+    if(!show){
+        alert('just do it')
+        mobileMenuToggler.innerHTML="CLOSE"
+        mobilemenu.classList.toggle('d-none')
+    }else{
+        mobileMenuToggler.innerHTML="MENU"
+    }
 })
